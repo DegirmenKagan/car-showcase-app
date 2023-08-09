2fa8da55611a6ca4fd33625c8285506f57b7db4e
Kağan Değirmen <kagandegirmen@gmail.com>
2023-08-09 23:25:53 +0300

    CarCard implemented, CustomButton props added

--- components/index.ts
+++ components/index.ts
@@ -6,6 +6,7 @@ import GitIconButton from './GitIconButton'
 import SearchBar from './SearchBar'
 import CustomFilter from './CustomFilter'
 import SearchManufacturer from './SearchManufacturer'
+import CarCard from './CarCard'
 
 export {
     Hero,
@@ -16,4 +17,5 @@ export {
     SearchBar,
     CustomFilter,
     SearchManufacturer,
+    CarCard,
 }
